0ea3d509e6f6d676b5b33585b4639448e1a4ec2d
Taras <drobotkot@gmail.com>
2019-07-12 13:38:01 +0300

    front-label-for-stock-info

---
+++ front-label-for-stock-info/front-label-for-stock-info.php
@@ -0,0 +1,61 @@
+<?php
+/*
+Plugin Name: Front label for stock info
+Plugin URI: https://github.com/TarasDrobotko/MyPortfolio/tree/master/front-label-stock-info
+Description: user friendly front label for stock info
+Author: Drobotko Taras
+Author URI: http://m662449k.beget.tech
+Version: 1.0.0
+ */
+
+/**
+ * Check if WooCommerce is active
+ */
+if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+
+    if (!class_exists('WC_StockInfo_Label')) {
+
+        /**
+         * Localisation
+         **/
+        load_plugin_textdomain('front-label-stock-info', false, dirname(plugin_basename(__FILE__)) . '/lang');
+
+        class WC_StockInfo_Label
+        {
+            public function __construct()
+            {
+
+                // WooCommerce Stock message
+                add_filter('woocommerce_get_availability', 'mw_get_availability', 10, 2);
+
+                function mw_get_availability($availability, $_product)
+                {
+                    global $product;
+                    $intro = __('Quantity in the store:', 'front-label-stock-info');
+
+                    if ($_product->is_in_stock() && $product->get_stock_quantity() > 0 && $product->get_stock_quantity() <= 2) {
+                        $availability['availability'] = $intro . ' ' . __('few', 'front-label-stock-info');
+                        return $availability;
+                    }
+
+                    if ($_product->is_in_stock() && $product->get_stock_quantity() > 2 && $product->get_stock_quantity() < 10) {
+                        $availability['availability'] = $intro . ' ' . __('middle', 'front-label-stock-info');
+                        return $availability;
+                    }
+
+                    if ($_product->is_in_stock() && $product->get_stock_quantity() >= 10) {
+                        $availability['availability'] = $intro . ' ' . __('many', 'front-label-stock-info');
+                        return $availability;
+                    } elseif (!$_product->is_in_stock()) {
+                        $availability['availability'] = $intro . ' ' . __('no', 'front-label-stock-info');
+                        return $availability;
+                    }
+
+                }
+            }
+        }
+
+        // finally instantiate our plugin class and add it to the set of globals
+        $GLOBALS['wc_stock_info_label'] = new WC_StockInfo_Label();
+    }
+}
---
+++ front-label-for-stock-info/readme.txt
@@ -0,0 +1,20 @@
+=== Front label for stock info ===
+Contributors: Taras
+Tags: woocommerce, label
+Stable tag: 1.0.0
+
+This is an example WooCommerce plugin
+
+== Description ==
+
+user friendly front label for stock info
+
+== Installation ==
+
+1. Upload the entire 'front-label-for-stock-info' folder to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Release
